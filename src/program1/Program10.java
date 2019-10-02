0325ac5e753890c65811806882e57b2872873215
kajolgupta <kajol.gupta_cs18@gla.ac.in>
2019-10-02 20:26:44 +0530

    init commit create program10class  add bufferclass  add input final commit

---
+++ src/program1/Program10.java
@@ -0,0 +1,22 @@
+package program1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Program10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        char c = (char) br.read();
+        int l = s.length();
+        System.out.println("output: ");
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == c) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
